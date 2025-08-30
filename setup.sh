d58efe11bac085697cd655a9859500d8ec3f935a
Richard Joseph(UST <richardj815165@gmail.com>
2025-08-30 09:28:52 +0530

    feat: k8s machine setup file

--- setup.sh
+++ setup.sh
@@ -1,8 +1,22 @@
-ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
-cat ~/.ssh/id_rsa.pub > /home/ubuntu/key.txt
-git config --global user.name "YOUR GITHUB USERNAME"
-
-
-snap install docker
-
-cat key.txt
+# Set hostname to kmaster
+sudo hostnamectl set-hostname kmaster
+ 
+# Initialize Kubernetes cluster
+sudo kubeadm init
+ 
+# Configure kubectl for current user
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+ 
+# Check the status of nodes
+kubectl get nodes
+ 
+# Apply Weave Net CNI plugin
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+ 
+# Check taints on kmaster node
+kubectl describe node kmaster | grep Taint
+ 
+# Remove control-plane taint to allow scheduling pods on master
+kubectl taint node kmaster node-role.kubernetes.io/control-plane:NoSchedule- || true
